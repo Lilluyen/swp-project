0c4a197f72bccdeeca6e970278576ff0bd7a3f06
Luyen <luyenlaluot@gmail.com>
2026-01-09 17:22:47 +0700

    first commit

---
+++ src/main/java/com/mycrm/se1992_nj_swp391_g2_crm/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycrm.se1992_nj_swp391_g2_crm;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ src/main/java/com/mycrm/se1992_nj_swp391_g2_crm/resources/JakartaEE10Resource.java
@@ -0,0 +1,20 @@
+package com.mycrm.se1992_nj_swp391_g2_crm.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee10")
+public class JakartaEE10Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
